6b1ac823810b3a0e8c73096205b284ec1a27b2ba
Andson lourenco <andson-lourenco@hotmail.com>
2026-03-31 20:21:29 -0300

    feat: create and use NotFoundError in controller.js

--- infra/controller.js
+++ infra/controller.js
@@ -2,6 +2,7 @@ import {
   InternalServerError,
   MethodNotAllowedError,
   ValidationError,
+  NotFoundError,
 } from "infra/errors.js";
 
 function onNoMatchHandle(request, response) {
@@ -10,7 +11,7 @@ function onNoMatchHandle(request, response) {
 }
 
 function onErrorHandle(error, request, response) {
-  if (error instanceof ValidationError) {
+  if (error instanceof ValidationError || error instanceof NotFoundError) {
     return response.status(error.statusCode).json(error);
   }
 
